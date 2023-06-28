7c72fd010bd8ee1dd003a236fe9995ad332c018e
François Mazeau <francois.mazeau@datadoghq.com>
2023-06-28 12:23:22 +0200

    WIP: additional decorators

--- preproc.py
+++ preproc.py
@@ -1,17 +1,45 @@
-from typing import Optional
+from typing import Optional, List
 import libcst as cst
 import yaml
 import collections
 
-def gen_decorators(releasedDic) -> 'dict[str, list]':
+
+# Set of helpers to generate verbose cst types
+
+def cstContextArg(ctxArg: str, val: str) -> cst.Arg:
+    return  cst.Arg(keyword=cst.Attribute(value=cst.Name("context"), attr=cst.Name(ctxArg)), value=cst.SimpleString(f'"{val}"'))
+
+def cstArg(arg: str, val: str) -> cst.Arg:
+    return cst.Arg(keyword=cst.Name(value=arg), value=cst.SimpleString(f'"{val}"'))
+
+def cstDecorator(funcName: str, args: List[cst.Arg]) -> cst.Decorator:
+    return cst.Decorator(decorator=cst.Call(
+        func=cst.Name(value=funcName),
+        args=args
+    ))
+
+def genReleasedDec(version):
+    args = [cst.Arg(keyword=cst.Name(value="golang"), value=cst.SimpleString(f'"{version}"'))]
+    return cstDecorator("released", args)
+
+def genIrrelevantDec(map):
+    reason = map["reason"]
+    args = [cstContextArg("library", "golang"), cstArg("reason", reason)]
+    return cst.Decorator(decorator=cst.Call(
+        func=cst.Name(value="irrelevant"),
+        args=args
+    ))
+
+def gen_decorators(yml) -> 'dict[str, list]':
     decs = collections.defaultdict(list)
-    for testName, version in releasedDic.items():
-        args = [cst.Arg(keyword=cst.Name(value="golang"), value=cst.SimpleString(f'"{version}"'))]
-        dec = cst.Decorator(decorator=cst.Call(
-            func=cst.Name(value="released"),
-            args=args
-        ))
-        decs[testName].append(dec)
+
+    for keyDec, decorators in yml.items():
+        for map in decorators:
+            testName = map["test_name"]
+            if keyDec == "released":
+                decs[testName].append(genReleasedDec(map["version"]))
+            elif keyDec == "irrelevant":
+                decs[testName].append(genIrrelevantDec(map))
 
     return decs
 
@@ -21,26 +49,23 @@ class MetadataWriter(cst.CSTTransformer):
         #TODO: store manifest parsed metadata
 
     def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node : cst.FunctionDef) -> cst.FunctionDef:
-        if not original_node.name.value.startswith("test_"):
-            return original_node
-        if original_node.name.value in released:
+        if original_node.name.value in decs:
+            print("True for " + original_node.name.value)
             return updated_node.with_changes(decorators=decs[original_node.name.value])
-
-        return original_node
+        return updated_node
 
     def leave_ClassDef(self, original_node: cst.ClassDef, updated_node : cst.ClassDef) -> cst.ClassDef:
-        if not original_node.name.value.startswith("Test_"):
-            return original_node
-        return updated_node.with_changes(decorators=decs[original_node.name.value])
+        if original_node.name.value in decs:
+            return updated_node.with_changes(decorators=decs[original_node.name.value])
+        return updated_node
 
 
 with open("manifest.yaml", "r") as yml_file:
     yml = yaml.safe_load(yml_file)
 
 
-released = {entry["test_name"]: entry["version"] for entry in yml["library"]["released"]}
-decs = gen_decorators(released)
-
+mapping={key: yml["golang"][key] for key in yml["golang"]}
+decs = gen_decorators(mapping)
 module = cst.parse_module(open("test_blocking_addresses.py").read()).visit(MetadataWriter())
 with open("modified.py", "w+") as out:
     out.write(module.code)
